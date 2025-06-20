e49f5fa669c8ebf706ff387bc512d09d817c24af
Brain17 <b.daminov@newuu.uz>
2025-06-20 17:03:05 +0500

    Add files via upload

---
+++ Lab9_no7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+     srand(time(0));
+     int arr[6]={};
+     for(int i=0;i<10000;i++){
+     arr [1+rand()%6-1] ++;
+     
+}
+   
+      for(int i=0;i<6;i++){
+        cout<<i+1<<":"<<arr[i]<<endl;
+    }
+      return 0;
+}
